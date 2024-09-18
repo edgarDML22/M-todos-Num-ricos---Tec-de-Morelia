fbe6d3f319c2baba5b2780d422a381dec8d75377
Edgar <edgar.ml.2005@outlook.com>
2024-09-18 08:53:54 -0600

    09/18

---
+++ JAVA/UNIDAD 2/ejemplo.java
@@ -0,0 +1,10 @@
+class ejemplo{
+    public static void main(String[] args) {
+        int n = 1;
+        switch (n) {
+            case 1 ->{
+                System.out.println("JIJIJA");
+            }
+        }
+    }
+}
